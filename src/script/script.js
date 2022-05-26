065d0d7fe7404c4bf78967e65511bb3951ca1514
luccajimenez <luccajbastos@gmail.com>
2022-05-26 14:05:30 -0300

    Uploading all files

---
+++ src/script/script.js
@@ -0,0 +1,35 @@
+const inputTexto = document.getElementById("texto");
+const inputBotao = document.getElementById("botao");
+const error = document.getElementById("error");
+const tarefas = document.querySelector('.panel-list');
+const panel = document.querySelector('.panel')
+
+tarefas.style.display = "none";
+
+let tarefasAdicionadas = [];
+
+function showError(msg) {
+    error.textContent = msg;
+    error.style.display = "block";
+    setTimeout(() => {
+       error.style.display = "none";
+    }, 4000);
+}
+
+function process(){
+    if(!inputTexto.value) {
+        showError("Coloque sua tarefa abaixo.");
+    } else {
+        tarefasAdicionadas.push(inputTexto.value);
+        tarefas.style.display = "block";
+        var div = document.createElement("div");
+        for(let i=0;i<tarefasAdicionadas.length;i++){
+            var conteudo = `<input type='checkbox' id="box"><span id='tarefa'>${tarefasAdicionadas[i]}</span>`;
+            panel.insertBefore(div, tarefas);
+            div.innerHTML = conteudo;
+        }
+    }
+}
+inputBotao.addEventListener('click', () => {
+    process();
+});
